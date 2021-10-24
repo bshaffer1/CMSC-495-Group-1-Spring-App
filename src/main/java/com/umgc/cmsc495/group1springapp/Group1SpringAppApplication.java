37497460ed86384803067d1120b006e477ddae78
getBrandon <brandonrussellshaffer@gmail.com>
2021-10-24 16:40:21 -0400

    Spring Maven commit

---
+++ src/main/java/com/umgc/cmsc495/group1springapp/Group1SpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.umgc.cmsc495.group1springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group1SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group1SpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umgc/cmsc495/group1springapp/Group1SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umgc.cmsc495.group1springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
