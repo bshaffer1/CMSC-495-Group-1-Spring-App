f11fb2bf4547aebefb4e8febc292aba4ed09c986
getBrandon <brandonrussellshaffer@gmail.com>
2021-11-13 19:09:51 -0500

    Initial commit of html controllers

---
+++ src/main/java/com/umgc/cmsc495/group1springapp/controllers/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.umgc.cmsc495.group1springapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Author: Brandon Shaffer
+ * Date: 11/13/2021
+ */
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+	public String helloWorld(HttpServletRequest request, Model model){
+		String name = request.getParameter("name");
+
+		if(name == null || name.isEmpty()){
+			name = "World!";
+		}
+
+		model.addAttribute("message", "Hello, " + name + "!");
+
+		return "hello-world";
+	}
+}
---
+++ src/main/java/com/umgc/cmsc495/group1springapp/controllers/WeatherDisplayController.java
@@ -0,0 +1,7 @@
+package com.umgc.cmsc495.group1springapp.controllers;
+/**
+ * Author: Brandon Shaffer
+ * Date: 11/13/2021
+ */
+public class WeatherDisplayController {
+}
